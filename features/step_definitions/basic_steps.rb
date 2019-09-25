de7dd6bc67597052ec40efb62600152bbd535e34
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-09-25 15:52:53 +0200

    Remove unused steps from basic_steps

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,10 +2,6 @@ When("I visit the {string} page") do |string|
   visit root_path
 end
 
-When("I click the {string} link") do |string|
-  click_link(string)
-end
-
 When("I fill in {string} with {string}") do |string, string2|
   fill_in(string, :with => string2)
 end
@@ -14,14 +10,7 @@ When("I click {string}") do |string|
   click_on(string)
 end
 
-When("I select {string} in {string}") do |string, string2|
-  select(string, :from => string2)
-end
-
 Then("I should see {string}") do |string|
-  expect(page).to have_content string
-end
-Then("I should not see {string}") do |string|
   expect(page).to have_content string
 end
 
@@ -36,15 +25,6 @@ Given("following users exists") do |table|
   end
 end
 
-Given("I am on the inbox page") do
-  visit mailbox_inbox_path
-end
-
-Given("I send mail to {string}") do |name|
-  @receiver = User.find_by(name:name )
-  @user.send_message(@receiver, 'Lorem ipsum...', 'Subject')
-end
-
 When("I select {string} named {string}") do |recipient, name|
   select name, from: recipient
 end
@@ -52,14 +32,3 @@ end
 Given("I visit the site") do
   visit root_path
 end
-
-
-=begin
-When("I visit the {string} page") do |string|
-  visit root_path
-end
-
-When("I click the {string} link") do |string|
-  click_link(string)
-end
-=end
