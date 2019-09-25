9e35aa5f30871b13fa6fb28835f03267f84ebde8
piavw <piavonwachenfelt@hotmail.com>
2019-09-25 18:56:37 +0200

    Updates step-def with counter-check.

--- features/step_definitions/inbox_read_mail_step.rb
+++ features/step_definitions/inbox_read_mail_step.rb
@@ -26,5 +26,5 @@ Given("I click the {string} button") do |string|
 end
 
 Then("I should see {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content string
 end
